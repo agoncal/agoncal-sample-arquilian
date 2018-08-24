04f9659d2ce908a3d001f18cb9465c211c7a14ed
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-08-24 21:45:37 +0200

    Adding Consul module

---
+++ 04-consul/README.md
@@ -0,0 +1,25 @@
+# Sample - Arquilian - Consul
+
+## Purpose of this sample
+
+The purpose of this sample is to show you how test a REST endpoint that needs Consul to register. So this sample shows:
+
+* Integration testing the REST endpoint and mocking Consul with Mockito
+* Integration testing the REST endpoint with Consul up and running
+
+[Read more on my blog](http://agoncal.wordpress.com/2012/01/16/wytiwyr-what-you-test-is-what-you-run/)
+
+## Compile and package
+
+Being Maven centric, you can compile and package it with `mvn clean compile`, `mvn clean package` or `mvn clean install`. The `package` and `install` phase will automatically trigger the unit tests. Once you have your war file, you can deploy it.
+
+## Execute the test
+
+The purpose of this sample is to execute unit and integration tests. So to execute it you can run :
+
+* `mvn test` : this will execute the unit test ItemEJBTest which uses Mockito
+* `mvn integration-test` : this will execute both integration tests `ItemEJBWithArquillianIT` and `ItemEJBWithoutArquillianIT`
+
+<div class="footer">
+    <span class="footerTitle"><span class="uc">a</span>ntonio <span class="uc">g</span>oncalves</span>
+</div>
---
+++ 04-consul/src/test/java/org/agoncal/sample/arquilian/consul/NumberResourceTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.agoncal.sample.arquilian.consul;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.swarm.arquillian.DefaultDeployment;
+
+import static io.restassured.RestAssured.get;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Arquillian.class)
+@RunAsClient()
+@DefaultDeployment()
+public class NumberResourceTest {
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
+    @Test
+    public void generateBookNumber() throws Exception {
+        Response response =
+                get("/api/numbers/book")
+                        .then()
+                        .extract().response();
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(response.asString()).startsWith("BK-");
+    }
+}
